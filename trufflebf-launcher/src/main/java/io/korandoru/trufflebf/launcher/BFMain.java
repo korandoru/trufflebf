5b8c69b0e75493b7664b9d0fc762b67b10676bb4
tison <wander4096@gmail.com>
2022-12-15 13:34:34 +0800

    feat: allow read source from STDIN
    Signed-off-by: tison <wander4096@gmail.com>

--- trufflebf-launcher/src/main/java/io/korandoru/trufflebf/launcher/BFMain.java
+++ trufflebf-launcher/src/main/java/io/korandoru/trufflebf/launcher/BFMain.java
@@ -2,6 +2,8 @@ package io.korandoru.trufflebf.launcher;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
@@ -11,7 +13,13 @@ public class BFMain {
     private static final String BF = "bf";
 
     public static void main(String[] args) throws IOException {
-        final Source source = Source.newBuilder(BF, new File(args[0])).build();
+        final Source source;
+        switch (args.length) {
+            case 0 -> source = Source.newBuilder(BF, new InputStreamReader(System.in), "<stdin>").build();
+            case 1 -> source = Source.newBuilder(BF, new File(args[0])).build();
+            default -> throw new IllegalArgumentException(Arrays.deepToString(args));
+        }
+
         final Context context = Context.newBuilder(BF)
                 .allowAllAccess(true)
                 .in(System.in)
@@ -19,8 +27,7 @@ public class BFMain {
                 .build();
         try (context) {
             final Value state = context.eval(source);
-            final Value result = state.execute();
-            System.out.println(result);
+            state.execute();
         }
     }
 
