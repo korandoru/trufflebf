a8a39f48ec3262586f547ea6fa26e0296a8d9c3c
tison <wander4096@gmail.com>
2023-01-01 14:06:16 +0800

    ci: Replace license checker
    Signed-off-by: tison <wander4096@gmail.com>

--- examples/interop.js
+++ examples/interop.js
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 const data = [72, 101, 108, 108, 111, 32, 102, 114, 111, 109, 32, 84, 114, 117, 102, 102, 108, 101, 66, 70, 32, 105, 110, 32, 74, 97, 118, 97, 83, 99, 114, 105, 112, 116, 46, 10]
 const program = Polyglot.eval('bf', '.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.')
 program(data, 0)
