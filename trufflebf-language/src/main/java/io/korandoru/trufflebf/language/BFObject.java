805f29552715c0e2a793f6172d08fbb9a66df44c
tison <wander4096@gmail.com>
2023-10-06 00:05:02 +0800

    build(deps): Bump GraalVM version
    Signed-off-by: tison <wander4096@gmail.com>

--- trufflebf-language/src/main/java/io/korandoru/trufflebf/language/BFObject.java
+++ trufflebf-language/src/main/java/io/korandoru/trufflebf/language/BFObject.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022-2023 Korandoru Contributors
+ * Copyright 2022 Korandoru Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -177,11 +177,10 @@ public class BFObject implements TruffleObject {
     @Override
     @CompilerDirectives.TruffleBoundary
     public String toString() {
-        return "BFObject{" +
-                "dp=" + dp +
-                ", ip=" + ip +
-                ", data=" + Arrays.toString(data.data) +
-                ", ins=" + ins.ins +
-                '}';
+        return "BFObject{" + "dp="
+                + dp + ", ip="
+                + ip + ", data="
+                + Arrays.toString(data.data) + ", ins="
+                + ins.ins + '}';
     }
 }
